206b33fc95476ffa43dc05f208dce27874e77d09
Youngchan <wdc4512@naver.com>
2025-01-21 12:37:02 +0900

    feat: 게시글 조회 기능 구현
    - 컨트롤러 작성
    - 서비스 작성 (메모리 데이터베이스 사용 상태)

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { Board } from './boards.entity';
 
 @Injectable()
 export class BoardsService {
-    
-    async hello(): Promise<string> {
-        return 'Hello From BoardsService';
+    // 테스트 용도로 boards배열을 임시 DB로 사용
+    private boards: Board[] = []
+
+    // 게시글 조회 기능
+    getAllBoards(): Board[] {
+        return this.boards
     }
 }
