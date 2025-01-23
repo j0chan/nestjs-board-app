c4c6f9b36ef558b0f568518ff28c991df88c56ef
Youngchan <wdc4512@naver.com>
2025-01-23 15:40:29 +0900

    chore: DB 설정 정보 중 port 숫자 형변환 필요

--- src/configs/database.config.ts
+++ src/configs/database.config.ts
@@ -6,7 +6,7 @@ export const databaseConfig = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PW,
-    port: +process.env.DB_PORT, //+는 문자->숫자로 바꿔줌
+    port: Number(process.env.DB_PORT), //+ 를 통해 문자->숫자
     database: process.env.DB_NAME,
     connectionLimit: 10,
     insecureAuth: true,
