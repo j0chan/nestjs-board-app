63244a68cf30892733d8684397c484eeae97a189
Youngchan <wdc4512@naver.com>
2025-01-23 17:45:32 +0900

    feat: Dto에 author 항목 추가

--- src/boards/dto/board-response.dto.ts
+++ src/boards/dto/board-response.dto.ts
@@ -1,10 +1,12 @@
 import { Board } from "../boards.entity"
 
 export class BoardResponseDto {
+    author: string
     title: string
     contents: string
 
     constructor(board: Board) {
+        this.author = board.author
         this.title = board.title
         this.contents = board.contents
     }
