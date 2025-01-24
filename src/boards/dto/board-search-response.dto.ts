1363acd5d1b2b2dcfaa54ee1caaebed7b28b3648
Youngchan <wdc4512@naver.com>
2025-01-24 16:41:27 +0900

    chore: ResponseDto 생성

--- src/boards/dto/board-search-response.dto.ts
+++ src/boards/dto/board-search-response.dto.ts
@@ -1,12 +1,13 @@
 import { Board } from "../boards.entity"
 
-export class BoardResponseDto {
+export class BoardSearchResponseDto {
+    author: string
     title: string
     contents: string
 
     constructor(board: Board) {
+        this.author = board.author
         this.title = board.title
         this.contents = board.contents
     }
-
 }
