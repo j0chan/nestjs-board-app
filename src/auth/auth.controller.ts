58302b393471dbfc0e5c2520c482dd48513deb8f
Youngchan <wdc4512@naver.com>
2025-02-04 12:26:25 +0900

    feat: 로그인 기능 기초 구조 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserResponseDto } from './dto/user-response.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('api/auth')
 export class AuthController {
@@ -12,5 +13,12 @@ export class AuthController {
     async createUser(@Body() createUserDto: CreateUserDto): Promise<UserResponseDto> {
         const userResponseDto = new UserResponseDto(await this.authService.createUser(createUserDto))
         return userResponseDto
+    }
+
+    // 로그인 기능
+    @Post('/signin')
+    async signIn(@Body() loginUserDto: LoginUserDto): Promise<string> {
+        const message = this.authService.signIn(loginUserDto)
+        return message
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,5 @@
-import { BadRequestException, ConflictException, Injectable } from '@nestjs/common';
+import { LoginUserDto } from './dto/login-user.dto';
+import { BadRequestException, ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './users.entity';
@@ -20,10 +21,8 @@ export class AuthService {
             throw new BadRequestException('Something went wrong.')
         }
 
-        // 이메일 중복 확인
         await this.checkEmailExist(email)
 
-        // 비밀번호 해싱
         const hashedPassword = await this.hashPassword(password)
 
         const newUser: User = {
@@ -37,13 +36,37 @@ export class AuthService {
         return createdUser;
     }
 
+    // 로그인
+    async signIn(loginUserDto: LoginUserDto): Promise<string> {
+        const { email, password } = loginUserDto
+
+        const existingUser = await this.findUserByEmail(email)
+
+        if (!existingUser || !(await bcrypt.compare(password, existingUser.password))) {
+            throw new UnauthorizedException('Invalid credentials')
+        }
+        const message = 'Login success'
+        return message
+    }
+
+    // 이메일 중복 확인
     async checkEmailExist(email: string): Promise<void> {
         const existingUser = await this.userRepository.findOne({ where: { email } })
         if (existingUser) {
             throw new ConflictException('Email already exists')
         }
     }
+    
+    // 이메일 존재 확인
+    async findUserByEmail(email): Promise<User> {
+        const existingUser = await this.userRepository.findOne({ where: { email } })
+        if (!existingUser) {
+            throw new NotFoundException('User not found')
+        }
+        return existingUser
+    }
 
+    // 비밀번호 해싱
     async hashPassword(password: string): Promise<string> {
         const salt = await bcrypt.genSalt() // 솔트 생성
         return await bcrypt.hash(password, salt) // 비밀번호 해싱
